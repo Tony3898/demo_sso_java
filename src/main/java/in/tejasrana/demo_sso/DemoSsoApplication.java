c873a4027d4459c5ddc3bf0cc40550387ee11f92
tejas <tejas@afreespace.com>
2022-04-07 17:08:33 +0530

    okta setup

---
+++ src/main/java/in/tejasrana/demo_sso/DemoSsoApplication.java
@@ -0,0 +1,13 @@
+package in.tejasrana.demo_sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/tejasrana/demo_sso/DemoSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tejasrana.demo_sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
